1b0d5b093bf99e109759f46f9d205b3204cdb519
Rathish <rathishvbr@gmail.com>
2025-04-20 09:15:16 +0200

    Added simple storage contract

--- deploy.js
+++ deploy.js
@@ -1,11 +1,39 @@
+const { ethers } = require('ethers');
+const fs = require('fs');
+require('dotenv').config();
+
 //compile the contract
 async function main() {
-    console.log("Hello World");
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(
+    process.env.PRIVATE_KEY,
+    provider,
+  )
+
+  const abi = JSON.parse(fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf-8'));
+  const binary = fs.readFileSync(
+    './SimpleStorage_sol_SimpleStorage.bin',
+    'utf-8',
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log('Deploying contract...')
+  const contract = await contractFactory.deploy();
+  await contract.deploymentTransaction().wait(1);
+  // Get number from contract
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current favorite number: ${currentFavoriteNumber}`);
+
+  // Store new number in contract
+  const tx = await contract.store(100);
+  await tx.wait();
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated favorite number: ${updatedFavoriteNumber}`);
 }
 
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+  })
