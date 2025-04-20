5d479e2ed3499415b657b9cfb0a6a3bd3db58844
Rathish <rathishvbr@gmail.com>
2025-04-20 09:42:47 +0200

    Create a script for encrypt key

--- deploy.js
+++ deploy.js
@@ -5,16 +5,15 @@ require('dotenv').config();
 //compile the contract
 async function main() {
   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(
-    process.env.PRIVATE_KEY,
-    provider,
-  )
-
-  const abi = JSON.parse(fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf-8'));
-  const binary = fs.readFileSync(
-    './SimpleStorage_sol_SimpleStorage.bin',
-    'utf-8',
+  const encryptedJsonKey = fs.readFileSync('./.encryptedKey.json', 'utf-8');
+  let wallet = ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJsonKey,
+    process.env.PRIVATE_KEY_PASSWORD,
   );
+  wallet = wallet.connect(provider);
+
+  const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf-8');
+  const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf-8');
 
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log('Deploying contract...')
